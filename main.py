8022570a1567a55b58e45e42034d461c6752a7c6
GrishaVoronin <voroningrisha2004@yandex.ru>
2021-10-11 23:28:40 +0300

    '1 Commit'

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(name)s - %(message)s', )
 
-if __name__ == '__main__':
+if name == 'main':
     from aiogram import executor
     from handlers import dp
 
