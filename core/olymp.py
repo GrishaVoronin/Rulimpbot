cf5ae638e16178fe56b2a9e51f840676a2787666
karchx <karch.andrus@gmail.com>
2021-10-13 13:16:31 +0300

    Implemented data objects

---
+++ core/olymp.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class OlympItem:
+    """Имплементирует олимпиаду из общего списка с небольшим описанием и главной информацией"""
+    link: str
+    olymp_title: str
+    about_info: str
+    date_info: str
+    rating: float
+
+    def __init__(self, link: str, olymp_title: str, about_info: str, date_info: str, rating: float):
+        self.link = link
+        self.olymp_title = olymp_title
+        self.about_info = about_info
+        self.date_info = date_info
+        self.rating = rating
+
+
+class FullOlympItem:
+    """Имлементирует все данные об олимпиаде (при полном открытии)"""
+    olymp_item: OlympItem
+    updates: list[str]
+    features: list[str]
+    preparation: list[dict]
+
+    def __init__(self, olymp_item: OlympItem, updates: list[str], features: list[str], preparation: list[dict]):
+        self.olymp_item = olymp_item
+        self.updates = updates
+        self.features = features
+        self.preparation = preparation
