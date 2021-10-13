9edea33cf20f7e58b26b2b6b6e61441309af461f
karchx <karch.andrus@gmail.com>
2021-10-13 12:30:55 +0300

    Добавление юзера в базу (aiosqlite)

---
+++ data/database/users_db.py
@@ -0,0 +1,15 @@
+import aiosqlite as lite
+
+data = 'data/database/data.db'
+
+
+async def add_new_user(user_id: int, user_name: str):
+    async with lite.connect(data) as con:
+        cur = await con.cursor()
+        await cur.execute("SELECT user_id FROM tbl_users WHERE user_id = ?", (user_id,))
+
+        if not (user_id,) in await cur.fetchall():
+            await cur.execute("INSERT INTO tbl_users(user_id, user_name)"
+                              "VALUES(?, ?)",
+                              (user_id, user_name))
+            await con.commit()
