9f4c86165ee7a51059b36b62c4b6214082c630b8
karchx <karch.andrus@gmail.com>
2021-10-13 12:30:55 +0300

    Добавление юзера в базу (aiosqlite)

--- handlers/users_handlers.py
+++ handlers/users_handlers.py
@@ -1,7 +1,10 @@
 from loader import dp, types
+from data.database import users_db as db
 from aiogram.types.chat import ChatType
 
 
 @dp.message_handler(ChatType.is_private, commands='start')
 async def send_welcome(message: types.Message):
-    await message.answer('Hello!')
+    user_id = message.from_user.id
+    user_name = message.from_user.username
+    await db.add_new_user(user_id, user_name)
